c5fb468b4f6e8dbacd52148fb1380f56bd4b287a
Valeria Echeveste <echevestevaleria.ev@gmail.com>
2025-10-18 17:23:52 -0300

    feat: view index/contacto y mapeo de URLS shop/

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request) :
+    return render(request, 'shop/index.html')
+    
+def contacto(request) :
+    return render(request, 'shop/contacto.html')
+  
