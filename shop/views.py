9ac8f408a7bb20ba5be44a055e835d785771b4ac
Valeria Echeveste <echevestevaleria.ev@gmail.com>
2025-10-19 19:50:59 -0300

    Vista nueva_remera creada e index actualizado para mostrar catálogo

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RemeraForm
+from .models import Remera
 
-# Create your views here.
+# --- Vistas principales ---
+def index(request):
+    remeras = Remera.objects.all()
+    return render(request, 'shop/index.html', {'remeras': remeras})
 
-def index(request) :
-    return render(request, 'shop/index.html')
-    
-def contacto(request) :
+def contacto(request):
     return render(request, 'shop/contacto.html')
-  
+
+# --- Vistas de catálogo ---
+def nueva_remera(request):
+    if request.method == 'POST':
+        form = RemeraForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = RemeraForm()
+    return render(request, 'shop/nueva_remera.html', {'form': form})
