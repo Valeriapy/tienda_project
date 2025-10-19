a580abb705c1843769f15678d50a03d0f7f7f6a1
Valeria Echeveste <echevestevaleria.ev@gmail.com>
2025-10-19 19:26:49 -0300

    Modelo Remera creado con campos marca, talle, color, lisa, género

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Remera(models.Model):
+    
+    TALLE_CHOICES = [
+        (1, 'XS'),
+        (2, 'S'),
+        (3, 'M'),
+        (4, 'L'),
+        (5, 'XL'),
+        (6, 'XXL'),
+    ]
+    
+    GENERO_CHOICES = [
+        (1, 'Hombre'),
+        (2, 'Mujer'),
+        (3, 'Unisex'),
+    ]
+    
+    marca = models.CharField(max_length=100)
+    talle = models.PositiveSmallIntegerField(choices=TALLE_CHOICES)
+    color = models.CharField(max_length=50)
+    lisa = models.BooleanField()
+    genero = models.PositiveSmallIntegerField(choices=GENERO_CHOICES)
+    
+    def __str__(self):
+        return f"{self.marca} - {self.get_talle_display()} - {self.color} - {'Lisa' if self.lisa else 'Estampada'} - {self.get_genero_display()}"
