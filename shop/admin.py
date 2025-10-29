32c420bbc6672d7875ab4263abb79a1638c6ff00
Valeria Echeveste <echevestevaleria.ev@gmail.com>
2025-10-29 20:13:35 -0300

    Etapa 3.2: Implementado panel de administración y eliminación de vista nueva_remera

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Remera
+
+@admin.register(Remera)
+class RemeraAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'talle', 'color', 'lisa', 'genero')
