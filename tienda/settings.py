6e94f2b3a42fd53830d6d3cd16f37233dcba5879
Valeria Echeveste <echevestevaleria.ev@gmail.com>
2025-10-30 17:00:40 -0300

    Configuro settings.py para despliegue en PythonAnywhere

--- tienda/settings.py
+++ tienda/settings.py
@@ -1,8 +1,92 @@
-from pathlib import Path
-import os
+"""
+Django settings for tienda project.
+Adaptado para despliegue en PythonAnywhere.
+"""
 
+import os
+from pathlib import Path
+
+# BASE_DIR
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+SECRET_KEY = "i#q4boflh%75m@yg-ug3owbm24bw$7uxk6zkh&#)0=3un7hmkr"
+
+# ⚠️ Cambiar a False en producción
+DEBUG = False
+
+
+ALLOWED_HOSTS = ["valeriapy.pythonanywhere.com", "localhost", "127.0.0.1"]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'shop', 
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+ROOT_URLCONF = 'tienda.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"], 
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'tienda.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+
+LANGUAGE_CODE = 'es-ar'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]      
+STATIC_ROOT = BASE_DIR / "staticfiles"        
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
